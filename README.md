d71af244a6cbe0e39d7590020a41fc85e478b4af
SosthenG <pro@sosthen.fr>
2023-01-05 19:16:22 +0100

    Add short access event subscription Will allow easy addition of other events

--- README.md
+++ README.md
@@ -3,14 +3,12 @@
 # yourls-notifier
 Plugin for [Yourls](https://yourls.org/) sending notifications.
 
-At the moment, this plugin only supports Discord and only send notifications when an url is shortened.
+At the moment, this plugin only supports Discord and only send notifications when an url is shortened or clicked (configurable).
 
 # How to install
-Like most Yourls plugins, just download the [latest release](https://github.com/SosthenG/yourls-notifier/releases/latest) and put it in the `user/plugins` directory.
-
-You should have at least the plugin.php file located as follow: `user/plugins/notifier/plugin.php`
+Like most Yourls plugins, just download the [latest release](https://github.com/SosthenG/yourls-notifier/releases/latest) and drop the `notifier` folder in the `user/plugins` directory.
 
 # How to use
 Go to the plugin configuration page and set your Discord webhook url. If you don't have one, you need to create it first.
 
-It's very simple, you can check Discord official documentation if you don't know how.
+It's very simple, you can check Discord [official documentation](https://support.discord.com/hc/en-us/articles/228383668-Intro-to-Webhooks) if you don't know how.
--- notifier/plugin.php
+++ notifier/plugin.php
@@ -3,7 +3,7 @@
 Plugin Name: Notifier
 Plugin URI: https://github.com/SosthenG/yourls-notifier
 Description: Sends a notification to Discord when a short is created
-Version: 1.0
+Version: 1.1
 Author: SosthenG
 Author URI: https://github.com/SosthenG
 */
@@ -13,7 +13,7 @@ if (!defined('YOURLS_ABSPATH')) {
     die();
 }
 
-function notify_discord(string $webhook, string $message, array $fields = [], ?DateTime $date = null)
+function notifier_discord(string $webhook, string $message, array $fields = [], ?DateTime $date = null)
 {
     if ($date === null) {
         $date = new \DateTime();
@@ -43,16 +43,17 @@ function notify_discord(string $webhook, string $message, array $fields = [], ?D
     }
 }
 
-yourls_add_filter('add_new_link', 'notify_new_link');
-function notify_new_link($data)
+function notifier_post_add_new_link($args)
 {
     $discord_webhook = yourls_get_option('notifier_discord_webhook');
     if (empty($discord_webhook)) {
-        return $data;
+        return;
     }
 
+    $data = $args[3];
+
     if ($data['status'] === 'success') {
-        notify_discord($discord_webhook, $data['message'], [
+        notifier_discord($discord_webhook, $data['message'], [
             [
                 'name' => 'Url',
                 'value' => $data['url']['url'],
@@ -70,18 +71,65 @@ function notify_new_link($data)
             ],
         ], new DateTime($data['url']['date']));
     }
-    
-    return $data;
+}
+
+function notifier_redirect_shorturl($args)
+{
+    $discord_webhook = yourls_get_option('notifier_discord_webhook');
+    if (empty($discord_webhook)) {
+        return;
+    }
+
+    notifier_discord($discord_webhook, 'Short "' . $args[1] . '" has just been used!', [
+        [
+            'name' => 'Short',
+            'value' => $args[1]
+        ],
+        [
+            'name' => 'Redirected to',
+            'value' => $args[0]
+        ],
+    ]);
+
 }
 
 yourls_add_action('plugins_loaded', 'notifier_loaded');
 function notifier_loaded()
 {
     yourls_register_plugin_page('notifier_settings', 'Notifier', 'notifier_register_settings_page');
+
+    $events = notifier_get_events_subscriptions();
+    foreach ($events as $event => $enabled) {
+        if ($enabled) {
+            yourls_add_action($event, 'notifier_' . $event);
+        }
+    }
+}
+
+
+function notifier_get_events_subscriptions() {
+    $events = [
+        'post_add_new_link' => true,
+        'redirect_shorturl' => false,
+    ];
+    $db_events = yourls_get_option('notifier_events_subscriptions');
+    foreach ($db_events as $event => $status) {
+        if (array_key_exists($event, $events)) {
+            $events[$event] = $status;
+        }
+    }
+
+    return $events;
 }
 
 function notifier_register_settings_page()
 {
+    $events = notifier_get_events_subscriptions();
+    $events_descriptions = [
+        'post_add_new_link' => 'When a new link is shortened',
+        'redirect_shorturl' => 'When a short URL is accessed',
+    ];
+
     if (isset($_POST['discord_webhook'])) {
         yourls_verify_nonce('notifier_settings');
 
@@ -91,6 +139,20 @@ function notifier_register_settings_page()
         } else {
             yourls_update_option('notifier_discord_webhook', $discord_webhook);
         }
+
+        $posted_events = [];
+        if (!empty($_POST['events'])) {
+            $posted_events = $_POST['events'];
+        }
+        foreach ($events as $event => $enabled) {
+            if (array_key_exists($event, $posted_events)) {
+                $events[$event] = true;
+            } else {
+                $events[$event] = false;
+            }
+        }
+
+        yourls_update_option('notifier_events_subscriptions', $events);
     }
 
     if (!isset($discord_webhook)) {
@@ -105,8 +167,18 @@ function notifier_register_settings_page()
             <input type="hidden" name="nonce" value="$nonce" />
             <p>
                 <label>Discord webhook url</label>
-                <input type="url" name="discord_webhook" value="$discord_webhook" placeholder="https://discord.com/api/webhooks/" />
+                <input type="url" name="discord_webhook" value="$discord_webhook" placeholder="https://discord.com/api/webhooks/" size="100" />
             </p>
+            <fieldset>
+                <legend>Events subscriptions</legend>
+HTML;
+
+    foreach ($events as $event => $enabled) {
+        echo '<input type="checkbox" id="' . $event . '" name="events[' . $event . ']" ' . ($enabled ? 'checked' : '') . '><label for="' . $event . '">' . $events_descriptions[$event] . '</label><br>';
+    }
+
+    echo <<<HTML
+            </fieldset>
             <p><input type="submit" value="Save" class="button" /></p>
             </form>
         </main>
